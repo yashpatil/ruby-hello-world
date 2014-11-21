a024069367a15ebd9b7ee94006b419fb0a229ff5
Yash Patil <yashdeep@gmail.com>
2014-11-21 14:06:40 +1100

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port,8080
 get '/' do
-  "FRIDAY\n"+
+  "Beer Time\n"+
 # ENV values are generated during template processing
 # and then passed to the container when openshift launches it.
   "User is #{ENV['ADMIN_USERNAME']}\n"+
